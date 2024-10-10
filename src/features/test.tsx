0d1ba7526c77593ee5a207b72d57adcdaf0709e9
danielpsrb <danielpsrb01@gmail.com>
2024-10-10 23:23:41 +0800

    first commit

---
+++ src/features/test.tsx
@@ -0,0 +1,9 @@
+export const TestComponent = () => {
+    return (
+        <div>
+        <p className="text-red-500 font-semibold">
+            Hello Test Component 123
+        </p>
+        </div>
+    );
+};
