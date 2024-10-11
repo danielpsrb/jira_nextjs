07c899e7b630e26a49e237d7e56e129f322f41f0
danielpsrb <danielpsrb01@gmail.com>
2024-10-11 12:23:50 +0800

    delete test components

--- src/features/test.tsx
+++
@@ -1,9 +0,0 @@
-export const TestComponent = () => {
-    return (
-        <div>
-        <p className="text-red-500 font-semibold">
-            Hello Test Component 123
-        </p>
-        </div>
-    );
-};
